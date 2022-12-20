b4c2a34d38da66ea7e432a5301a9c82212033a20
liigo <liiigo@outlook.com>
2022-12-20 23:13:32 +0800

    Initial Commit

---
+++ BookList_Practice/BookList_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookList_PracticeApp.swift
+//  BookList_Practice
+//
+//  Created by 齐一凡 on 2022/12/20.
+//
+
+import SwiftUI
+
+@main
+struct BookList_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
